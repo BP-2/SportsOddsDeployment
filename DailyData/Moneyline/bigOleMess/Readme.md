9391a39a1f77407847a259cd48b56d9f7e4116a8
Brady Phelps <bp309420@ohio.edu>
2025-01-21 13:47:53 -0500

    Point swap fix

--- DailyData/Moneyline/bigOleMess/Readme.md
+++ DailyData/Moneyline/bigOleMess/Readme.md
@@ -5,6 +5,7 @@
 3. Attach the full names by running fixAutoOdds.py. 
 4. Make sure you have a csv file with the game data from basketball reference that has matching games.
 5. Pass both files to doubleMergeNew.py
+**Note: PTS mighted be swapped. If so, run swapPtsIssue.py on resulting data.**
 
 There is a lot of extra mess in here, because I was generating a big data set (cleaned_data.csv) with all
 game results and odds, but I did not have entire data of odds. This lead to me dealing with a bunch of data
---
+++ DailyData/Moneyline/bigOleMess/swapPtsIssue.py
@@ -0,0 +1,19 @@
+import csv
+
+input_file = 'allPtsIncorrect.csv'
+output_file = 'allPtsCorrected.csv'
+
+with open(input_file, mode='r', newline='', encoding='utf-8') as infile, \
+     open(output_file, mode='w', newline='', encoding='utf-8') as outfile:
+
+    reader = csv.DictReader(infile)
+    fieldnames = reader.fieldnames  
+    writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+
+    for row in reader:
+        row['PTS'], row['PTS2'] = row['PTS2'], row['PTS']
+        writer.writerow(row)
+
+print(f"Swapped PTS and PTS2 successfully. Output saved to {output_file}.")
